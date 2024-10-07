afdcb123b86921eaa217ef249091e0e37d5428b4
Junior flores <fmjg21@gmail.com>
2024-10-07 16:53:17 -0500

    order cache

--- src/org/openstreetmap/josm/plugins/devseed/JosmMagicWand/utils/SamImageGrid.java
+++ src/org/openstreetmap/josm/plugins/devseed/JosmMagicWand/utils/SamImageGrid.java
@@ -8,6 +8,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class SamImageGrid extends JPanel implements ImagePanelListener {
     ObjectMapper objectMapper = new ObjectMapper();
@@ -22,6 +26,17 @@ public class SamImageGrid extends JPanel implements ImagePanelListener {
         //
         File cacheDir = new File(CommonUtils.magicWandCacheDirPath());
         File[] cacheFiles = cacheDir.listFiles((dir, fileName) -> fileName.endsWith(".json"));
+        if (cacheFiles != null) {
+            Arrays.sort(cacheFiles, new Comparator<File>() {
+                @Override
+                public int compare(File file1, File file2) {
+                    LocalDateTime dateTime1 = extractDateTimeFromFileName(file1.getName());
+                    LocalDateTime dateTime2 = extractDateTimeFromFileName(file2.getName());
+                    return dateTime2.compareTo(dateTime1);
+                }
+            });
+        }
+
 
         for (File jsonFile : cacheFiles) {
             try {
@@ -32,9 +47,8 @@ public class SamImageGrid extends JPanel implements ImagePanelListener {
             }
         }
     }
-
     public void addSamImage(SamImage samImage) {
-        if (samImage.isEncode()) {
+        if (samImage.isEncode() && !samImage.getImageUrl().isEmpty()) {
             samImageList.add(samImage);
         }
         updateJpanel();
@@ -67,6 +81,22 @@ public class SamImageGrid extends JPanel implements ImagePanelListener {
         repaint();
     }
 
+    private LocalDateTime extractDateTimeFromFileName(String fileName) {
+        try {
+            // '2024_10_07__12_17_22__farm.json'
+            String[] parts = fileName.split("__");
+            String datePart = parts[0]; // '2024_10_07'
+            String timePart = parts[1]; // '12_17_22'
+
+            String dateTimeString = datePart + " " + timePart;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM_dd HH_mm_ss");
+
+            return LocalDateTime.parse(dateTimeString, formatter);
+        } catch (Exception e) {
+            Logging.error("Error parsing date from file name: " + fileName);
+            return LocalDateTime.MIN;
+        }
+    }
 
     @Override
     public void removeSamImage(SamImage samImage) {
